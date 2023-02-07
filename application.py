a47a7103f78f5447d9f36c22e29d19b2afe93812
AndyR-001 <120258669+AndyR-001@users.noreply.github.com>
2023-02-07 11:36:39 +0000

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,6 @@ print("Content-Type: text/html\n")
 import mysql.connector
 from flask import Flask, request, redirect, render_template, url_for, session
 import os
-os.environ["FLASK_APP"] = "application.py"
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -391,4 +390,4 @@ def results():
 #Start Flask application in debug mode   
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run()
